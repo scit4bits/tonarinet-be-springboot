0f3a2a7d0d41c2cdf9e2112c59cb3806155a7bba
hjs0410hc <s7s7111@naver.com>
2025-08-12 10:42:57 +0900

    first commit

---
+++ src/main/java/org/scit4bits/tonarinetserver/TonarinetserverApplication.java
@@ -0,0 +1,13 @@
+package org.scit4bits.tonarinetserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TonarinetserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TonarinetserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/scit4bits/tonarinetserver/TonarinetserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scit4bits.tonarinetserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TonarinetserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
